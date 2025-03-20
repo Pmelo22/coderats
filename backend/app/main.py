1921fe41d8f48b8811a6df2427e939e65cc1ae55
Patrick <75812309+Pmelo22@users.noreply.github.com>
2025-03-20 12:11:04 -0300

    pre-alpha

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from app.routes import auth
 from app.routes import ranking
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.include_router(auth.router)
+
 app.include_router(ranking.router)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Permitir todas as origens (mude para ["http://localhost:3000"] se necessário)
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
--- backend/app/routes/ranking.py
+++ backend/app/routes/ranking.py
@@ -1,19 +1,9 @@
-from fastapi import APIRouter, Depends
-import requests
-from sqlalchemy.orm import Session
-from app.database import SessionLocal
-from app.models import User, Ranking
+from fastapi import APIRouter
+from ..database import get_rankings
 
 router = APIRouter()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 @router.get("/ranking")
-def get_ranking(db: Session = Depends(get_db)):
-    rankings = db.query(Ranking).order_by(Ranking.commits.desc()).all()
-    return [{"username": r.user.username, "commits": r.commits} for r in rankings]
+async def read_rankings():
+    """ Rota para obter os rankings """
+    return {"rankings": get_rankings()}

23b541b5941500739856be3927d5557e3fc14e47
Patrick <75812309+Pmelo22@users.noreply.github.com>
2025-03-20 21:54:55 -0300

    remove backend

--- backend/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.routes import ranking
-
-app = FastAPI()
-
-app.include_router(ranking.router)
--- backend/app/routes/ranking.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from ..database import get_rankings
-
-router = APIRouter()
-
-@router.get("/ranking")
-async def read_rankings():
-    """ Rota para obter os rankings """
-    return {"rankings": get_rankings()}

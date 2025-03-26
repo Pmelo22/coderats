068827fd8a045ac13a978d9b63acde3106e4202a
Patrick <75812309+Pmelo22@users.noreply.github.com>
2025-03-26 14:52:32 -0300

    Revert "Update index.tsx"
    This reverts commit 17638b718eda58650236e19cb7f75327a6616d9c.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,25 +1,39 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import "../styles/globals.css"; // Importando estilos globais
+import Image from 'next/image'
+
 
 export default function Home() {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  // Redireciona para /ranking após login
+  useEffect(() => {
+    if (session) {
+      router.push("/ranking");
+    }
+  }, [session, router]);
+
   return (
     <div className="container">
       <header className="header">
-        <img className="logo" src="/logo.png" alt="Logo Coderats" />
+      <Image src="/logo.png" alt="Logo" width={200} height={200} />
       </header>
       <main className="main-content">
         <h1>Coderats 🚀</h1>
-        {session ? (
-          <>
-            <p>Bem-vindo, {session.user?.name}!</p>
-            <button className="btn" onClick={() => signOut()}>Sair</button>
-          </>
-        ) : (
+        {!session ? (
           <div className="button-container">
-            <button className="btn" onClick={() => signIn("github")}>Entrar com GitHub</button>
-            <button className="btn" onClick={() => window.location.href = "/ranking"}>Acessar Ranking</button>
+            <button className="btn" onClick={() => signIn("github")}>
+              Entrar com GitHub
+            </button>
+            <button className="btn" onClick={() => router.push("/ranking")}>
+              Acessar Ranking
+            </button>
           </div>
+        ) : (
+          <p>Redirecionando...</p>
         )}
       </main>
     </div>
